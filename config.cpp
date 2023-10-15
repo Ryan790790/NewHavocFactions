9bc495f5f1eb17c08727251cbed3e3e4951b66f1
Ryan790790 <Ryan790790@gmail.com>
2023-10-15 15:42:37 -0400

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,34 +1,33 @@
-#define _ARMA_
-
 class CfgPatches
 {
-	class DayZ_Havoc_Expansion_Factions_scripts
-	{
-		requiredAddons[] = {};
-	};
+    class DayZ_Havoc_Expansion_Factions_scripts
+    {
+        requiredAddons[] = {"DayZExpansion_AI_Scripts"};
+    };
 };
 class CfgMods
 {
-	class Havoc_More_Factions
-	{
-		action = "";
-		hideName = 0;
-		hidePicture = 0;
-		name = "More Expansion Factions";
-		credits = "DayZ Expansion and Schleeb";
-		author = "Schleeb";
-		authorID = "";
-		version = "0.1";
-		extra = 0;
-		type = "servermod";
-		dependencies[] = {"Game"};
-		class defs
-		{
-			class gameScriptModule
-			{
-				value = "";
-				files[] = {"NewHavocPack/Scripts/3_Game"};
-			};
-		};
-	};
+    class Havoc_More_Factions
+    {
+        dir = "NewHavocFactions";
+        action = "";
+        hideName = 0;
+        hidePicture = 0;
+        name = "More Expansion Factions";
+        credits = "DayZ Expansion and Schleeb";
+        author = "Schleeb";
+        authorID = "";
+        version = "0.1";
+        extra = 0;
+        type = "mod";
+        dependencies[] = { "Game", "World", "Mission" };
+        class defs
+        {
+            class gameScriptModule
+            {
+                value = "";
+                files[] = {"NewHavocFactions/Scripts/3_Game"};
+            };
+        };
+    };
 };
